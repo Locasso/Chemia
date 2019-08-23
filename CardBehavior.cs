8b526c1ad89eb1ac97aa6582a637568df5d0d1d2
Gabriel Locasso <gabriel_locasso@hotmail.com>
2019-08-23 00:30:09 -0300

    First Commit

---
+++ CardBehavior.cs
@@ -0,0 +1,377 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.EventSystems;
+
+public class CardBehavior : MonoBehaviour {
+
+    public enum CardColor
+    {
+        LARANJA,
+        VERMELHO,
+        MARROM,
+        AMARELO,
+        UNCOLOR
+    }
+
+    [Header("Sprite")]
+    public Sprite mySprite;
+    private Dictionary<string, Sprite> dictSprites = new Dictionary<string, Sprite>(); //Dicionário de cartas na mão
+    private Dictionary<string, Sprite> tableSprites = new Dictionary<string, Sprite>(); //Dicionário de cartas da mesa(Sem cor, só com nome)
+    public Vector3 scaleUp, scaleDown;
+
+    [Header("Controladores")]
+    public float random;
+    public bool isPlayer, isDeck, isNotPlayer, isTable;
+    public List<int> ints;
+    public bool canInteract; //Controla se a carta já pode interagir com a carta que está na mesa
+    public bool hasReleased, hasReleasedInTrigger; //Ativa quando o player solta o botão do mouse, para controle de algumas funções.
+    public Vector2 positionToChange; //A variável que controlará a posição do player durante o game.
+    public float speedToTranslate; //A variável que controla o speed que a carta irá se mover de volta para sua posição inicial.
+
+    GameManager gameManager;
+    Quaternion quart; //Pequena Rotação que a carta terá quando for colocada na mesa
+
+    [Header("Status")]
+    public CardColor corCarta;
+    public float valorCarta;
+    public bool isSpecial;
+
+    [Header("Variáveis Movimento")]
+    public Vector2 startPosition;
+
+    [Header("Componentes de Áudio")]
+    public AudioSource audioSource;
+    public AudioClip audioClip;
+
+    public Dictionary<string, Sprite> DictSprites
+    {
+        get
+        {
+            return  dictSprites;
+        }
+        set
+        {
+            dictSprites = value;
+        }
+    }
+    public Dictionary<string, Sprite> TableSprites
+    {
+        get
+        {
+            return tableSprites;
+        }
+        set
+        {
+            tableSprites = value;
+        }
+    }
+
+    [Header("Visuais")]
+    public ParticleSystem CardPut, CardBuy;
+
+    void Start ()
+    {
+        speedToTranslate = 5;
+        quart = new Quaternion(this.gameObject.transform.rotation.x, this.gameObject.transform.rotation.y, this.transform.rotation.z + Random.Range(-0.15f, 0.15f), this.gameObject.transform.rotation.w);
+        audioSource = GetComponent<AudioSource>();
+        audioSource.volume = 1.0f;
+        this.scaleDown = this.gameObject.transform.localScale;
+        scaleUp = new Vector3(0.6f, 0.6f, 0.6f);
+        gameManager = GameObject.FindObjectOfType<GameManager>();
+        this.gameObject.transform.parent = GameObject.Find("Cards").transform;
+        GetRandomNumber();
+        DefineCard();
+        CardsStats();
+        this.startPosition = this.gameObject.transform.position;
+        CardPut = GetComponentInChildren<ParticleSystem>();
+    }
+    private void Update()
+    {
+        if(isTable && gameManager.turnTheCard)
+        {
+            GetComponent<SpriteRenderer>().sprite = tableSprites["cards2_" + random];
+            this.gameObject.transform.rotation = quart;
+            this.gameObject.tag = "Table";
+            this.transform.position = gameManager.firstInTablePosition.transform.position;
+        }
+        else if (isTable && gameManager.turnTheCard == false)
+            GetComponent<SpriteRenderer>().sprite = tableSprites["cards_" + random];
+    }
+
+    private void OnMouseEnter()
+    {
+                if (isPlayer || isTable)
+            {
+                this.gameObject.transform.localScale = scaleUp;
+                this.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 1;
+                audioClip = Resources.Load<AudioClip>("chipLay");
+                audioSource.clip = audioClip;
+                audioSource.Play();
+            }
+    }
+    private void OnMouseExit()
+    {
+        this.gameObject.transform.localScale = scaleDown;
+        this.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 0;
+       
+    }
+    void OnMouseDown()
+    {
+        if (gameManager.state != GameManager.Turn.PLAYER_1 || gameManager.opponentTurn)
+            if (EventSystem.current.IsPointerOverGameObject())
+            {
+                return;
+            }
+        if (isDeck)
+            {
+            GetComponent<SpriteRenderer>().sprite = dictSprites["cards_" + random];
+            CardPut.Play();
+            this.startPosition = gameManager.players[0].transform.position;
+            this.gameObject.transform.position = gameManager.players[0].gameObject.transform.position;
+            audioClip = Resources.Load<AudioClip>("cardSlide");
+            audioSource.clip = audioClip;
+            audioSource.Play();
+            gameManager.cardInDeck.Remove(this.gameObject);
+            gameManager.cardsInTable += 1;
+            gameManager.playerHand.Add(this.gameObject);
+            if (this.gameManager.cardInTable.GetComponent<CardBehavior>().valorCarta == this.valorCarta
+               || this.gameManager.cardInTable.GetComponent<CardBehavior>().corCarta == this.corCarta
+               || this.gameManager.cardInTable.GetComponent<CardBehavior>().isSpecial && this.corCarta == CardColor.UNCOLOR
+               || this.isSpecial && this.corCarta == CardColor.UNCOLOR) 
+            {
+                this.gameObject.transform.position = gameManager.cardInTable.gameObject.transform.position;
+                canInteract = true;
+                this.isPlayer = true;
+                this.isDeck = false;
+            }
+            else
+            {
+                gameManager.StartCoroutine(gameManager.StateMachine());
+                gameManager.idTurn = 2;
+                gameManager.PutInTable();
+                this.isPlayer = true;
+                this.isDeck = false;
+            }  
+        }
+            if (isPlayer)
+                canInteract = true;
+                audioClip = Resources.Load<AudioClip>("cardSlide");
+                audioSource.clip = audioClip;
+                audioSource.Play();
+				//else if(isTable)
+				//    GetComponent<SpriteRenderer>().sprite = dictSprites["cards_" + random];
+    }
+    void OnMouseDrag()
+    {
+        if (gameManager.state != GameManager.Turn.PLAYER_1 || gameManager.opponentTurn)
+            if (EventSystem.current.IsPointerOverGameObject())
+            {
+                return;
+            }
+        if (isPlayer)
+        {
+            float distance_to_screen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+            Vector3 pos_move = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen));
+            transform.position = new Vector3(pos_move.x, pos_move.y, pos_move.z);
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        if (gameManager.state != GameManager.Turn.PLAYER_1 || gameManager.opponentTurn)
+            if (EventSystem.current.IsPointerOverGameObject())
+            {
+                return;
+            }
+        if (isPlayer)
+        {
+            if (hasReleasedInTrigger)
+                hasReleased = true;
+            //this.transform.position = startPosition;
+            audioClip = Resources.Load<AudioClip>("cardPlace");
+            audioSource.clip = audioClip;
+            audioSource.Play();        
+        }
+    }
+    public void TransformToStartPosition()
+    {
+        float speed = speedToTranslate * Time.deltaTime;
+        this.gameObject.transform.position = Vector3.MoveTowards(transform.position, startPosition, speed);
+    }
+
+    public void DefineCard()
+    {
+        Sprite[] sprites = Resources.LoadAll<Sprite>("cards");
+        Sprite[] spritesTable = Resources.LoadAll<Sprite>("cards2");
+
+        foreach (Sprite sprite in sprites)
+        {
+            dictSprites.Add(sprite.name, sprite);
+        }
+        foreach (Sprite sprite in spritesTable)
+        {
+            tableSprites.Add(sprite.name, sprite);
+        }
+
+        if (isPlayer)
+        {          
+            GetComponent<SpriteRenderer>().sprite = dictSprites["cards_" + random];
+            this.gameObject.tag = "Player";
+            gameManager.playerCards.Add(this.gameObject);
+            gameManager.playerHand.Add(this.gameObject);
+        }
+        else if (isNotPlayer)
+        {
+            gameManager.oponentHand.Add(this.gameObject);
+            GetComponent<SpriteRenderer>().sprite = dictSprites["cards_" + 41];
+        }
+        else
+            GetComponent<SpriteRenderer>().sprite = dictSprites["cards_" + 41];
+    }
+    public float GetRandomNumber()
+    {
+        do random = Random.Range(0, 40);
+        while (gameManager.cardUsed.Contains(random));
+        gameManager.cardUsed.Add(random);
+        return random;
+    }
+    public void CardsStats()
+    {
+        if(random >= 0 && random <= 8)
+        {
+            this.corCarta = CardColor.LARANJA;
+            valorCarta = random +2;
+            if(valorCarta == 10)
+            {
+                valorCarta = 0;
+            }
+        }
+        else if (random >= 9 && random <= 17)
+        {
+            this.corCarta = CardColor.VERMELHO;
+            valorCarta = random - 7;
+            if (valorCarta == 10)
+            {
+                valorCarta = 0;
+            }
+        }
+        else if (random >= 18 && random <= 26)
+        {
+            this.corCarta = CardColor.MARROM;
+            valorCarta = random - 16;
+            if (valorCarta == 10)
+            {
+                valorCarta = 0;
+            }
+        }
+        else if (random >= 27 && random <= 35)
+        {
+            this.corCarta = CardColor.AMARELO;
+            valorCarta = random - 25;
+            if (valorCarta == 10)
+            {
+                valorCarta = 0;
+            }
+        }
+        else
+        {
+            isSpecial = true;
+            this.corCarta = CardColor.UNCOLOR;
+        }
+    }
+
+    //public void OnTriggerStay2D(Collider2D collision)
+    //{
+    //    if (collision.gameObject.tag == "Table" && canInteract)
+    //    {
+    //        if (collision.gameObject.GetComponent<CardBehavior>().valorCarta == this.valorCarta || collision.gameObject.GetComponent<CardBehavior>().corCarta == this.corCarta
+    //            || this.isSpecial && corCarta == CardColor.UNCOLOR)
+    //        {
+    //            TransformToStartPosition();
+    //        }
+
+    //        hasReleasedInTrigger = true;    
+    //    }
+    //}
+
+    //public void OnTriggerExit2D(Collider2D collision)
+    //{
+    //    if (collision.gameObject.tag == "Table" && canInteract)
+    //    {
+    //        hasReleasedInTrigger = false;
+    //    }
+    //}
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Table" && canInteract)
+        {
+                if (collision.gameObject.GetComponent<CardBehavior>().valorCarta == this.valorCarta 
+                || collision.gameObject.GetComponent<CardBehavior>().corCarta == this.corCarta
+                || this.isSpecial && corCarta == CardColor.UNCOLOR 
+                || collision.gameObject.GetComponent<CardBehavior>().isSpecial 
+                && collision.gameObject.GetComponent<CardBehavior>().corCarta == CardColor.UNCOLOR)
+                {
+                if (isPlayer)
+                {
+                    this.gameObject.tag = "Table";
+                    this.isPlayer = false;
+                    this.isTable = true;
+                    this.canInteract = false;
+                    //this.startPosition = collision.gameObject.transform.position;
+                    gameManager.cardUsed.Remove(collision.gameObject.GetComponent<CardBehavior>().random);
+                    gameManager.cardsInTable -= 1;
+                    Destroy(collision.gameObject);
+                    Debug.Log("Match");
+                    hasReleased = false;
+                    Debug.Log("EnemyTurn");
+                    this.gameManager.cardInTable = this.gameObject;
+                    gameManager.StartCoroutine(gameManager.StateMachine());
+                    gameManager.idTurn = 2;
+                    gameManager.PutInTable();
+                    gameManager.playerHand.Remove(this.gameObject);
+                    CardPut.Play();
+                }
+                else if(isNotPlayer)
+                {
+                    audioClip = Resources.Load<AudioClip>("cardPlace");
+                    audioSource.clip = audioClip;
+                    audioSource.Play();
+                    this.gameObject.tag = "Table";
+                    this.isDeck = false;
+                    this.isNotPlayer = false;
+                    this.canInteract = false;
+                    this.isPlayer = false;
+                    this.isTable = true;
+                    //this.startPosition = collision.gameObject.transform.position;
+                    gameManager.cardUsed.Remove(collision.gameObject.GetComponent<CardBehavior>().random);
+                    gameManager.cardsInTable -= 1;
+                    Destroy(collision.gameObject);
+                    this.gameManager.cardInTable = this.gameObject;
+                    this.gameObject.transform.rotation = collision.transform.rotation;
+                    CardPut.Play();
+                    //collision.gameObject.SetActive(false);
+                    //collision.gameObject.transform.Translate(new Vector2(20, 0));
+                    Debug.Log("Match");
+                    Debug.Log("PlayerTurn");
+                    gameManager.state = GameManager.Turn.PLAYER_1;
+
+                }
+                    //for(int i = 0; i <= gameManager.playerCards.Count; i++)
+                    //{
+                    //    if(gameManager.playerCards[i] > )
+                    //}
+                }
+
+                else
+                {
+                    gameManager.invalidMatch = true;
+                    gameManager.MatchsControl();
+                    Debug.Log("Unmatch");
+                    hasReleased = false;
+                }        
+        }
+    }
+}
---
+++ GameManager.cs
@@ -0,0 +1,292 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GameManager : MonoBehaviour {
+
+    public enum Turn
+    {
+        PLAYER_1,
+        PLAYER_2,
+        PLAYER_3,
+        PLAYER_4,
+        GAMEOVER
+    }
+
+    public enum Phases
+    {
+        BEGIN,
+        MIDDLE,
+        END
+    }
+
+    [Header("Player no Jogo")]
+    public GameObject[] players;
+
+    [Header("Objetos")]
+    public GameObject card, deck, firstInTablePosition;
+    public GameObject cardInTable; //Guarda o card atual que está na mesa;
+    public GameObject cardsHold; //Segura todas as cartas do jogo!
+
+    [Header("Lista de Cards Usados")]
+    public List<float> cardUsed;
+    public List<GameObject> cardInDeck;
+    public GameObject firstInTable;
+    public int cardsInTable;
+
+    [Header("Lista de Cards do Player")]
+    public List<GameObject> playerCards;
+    public List<GameObject> playerHand; //Lista de cartas na mão do jogador;
+
+    [Header("Lista de Cards do Oponente")]
+    public List<GameObject> oponentHand;
+
+    [Header("Utilitários")]
+    public float howManyAdded; // How many cards I added so far
+    float gapFromOneItemToTheNextOne; //the gap I need between each card
+    public int howManyToStop;
+    public bool gameBegin = true; //Checa se é o começo da partida;
+    public bool gameOver = false; //Checa se é o fim do jogo.
+    public bool hasPlayed;
+
+    [Header("Lógicas dos Turnos")]
+    public Turn state; 
+    public int idTurn = 1;
+    public bool turnTheCard; //Bollean para controlar o sprite das cartas na mesa.
+    public Phases phases; //Enum que controla as fases de jogo.
+    public int idPhases; //Int que controla as fases do jogo.
+    public bool opponentTurn;
+
+    [Header("Lógica de Vitórias")]
+    public int p1_point, p2_points;
+
+    [Header("Feedbacks")]
+    public GameObject invalidNot; //Gameobject de match inválido
+    public bool invalidMatch;
+
+    [Header("Visuais e Animações")]
+    public Animator myTurn, oponentTurn;
+    public GameObject victory, defeat;
+
+    [Header("Componentes de Áudio")]
+    public AudioSource audioSource;
+    public AudioClip audioClip;
+
+    void Start ()
+    {
+        audioSource = GetComponent<AudioSource>();
+        victory.SetActive(false);
+        defeat.SetActive(false);
+        idTurn = Random.Range(1, 2);        
+        howManyAdded = 0.0f;
+        gapFromOneItemToTheNextOne = 1.0f;
+        card = Resources.Load<GameObject>("Prefabs/card");
+        SpawnCards(7, card);
+        SpawnDeck(40, card);
+        MatchsControl();
+        PutInTable();
+        cardInTable = firstInTable;
+        cardsHold = GameObject.Find("Cards");
+    }
+	
+	void Update ()
+    {
+        FitCards();
+        if(!gameOver)
+        TryCards();
+
+        if(cardInDeck.Count <= 0)
+        {
+            SpawnDeck(40, card);
+        }
+        if(playerHand.Count == 0)
+        {
+            gameOver = true;
+            victory.SetActive(true);
+            state = Turn.GAMEOVER;
+            cardsHold.SetActive(false);
+            Debug.Log("Player Wins");
+            StartCoroutine(BackToMenu());
+        }
+        else if(oponentHand.Count == 0)
+        {
+            gameOver = true;
+            defeat.SetActive(true);
+            state = Turn.GAMEOVER;
+            cardsHold.SetActive(false);
+            Debug.Log("Enemy Wins");
+            StartCoroutine(BackToMenu());
+        }
+	}
+    public void MatchsControl()
+    {
+        //Notificação de carta inválida
+        invalidNot.SetActive(false);
+        if(invalidMatch)
+        {
+            invalidNot.SetActive(true);
+            invalidMatch = false;
+        }
+    }
+
+    public void FitCards()
+    {
+        if (playerCards.Count == 0) //Se a lista for nula, para a função
+            return;
+        GameObject obj = playerCards[0]; //Referência da primeira imagem na minha lista
+        obj.transform.position = players[0].transform.position;
+        Vector3 handPosition = obj.transform.position;
+        handPosition += new Vector3((howManyAdded * gapFromOneItemToTheNextOne), 0, 0); // Move minha carta 1f para a direita
+        obj.transform.position = handPosition;
+        obj.GetComponent<CardBehavior>().startPosition = handPosition;
+        obj.GetComponent<CardBehavior>().isDeck = false;
+        playerCards.RemoveAt(0);
+        howManyAdded++;
+    }
+
+    public void SpawnCards(int numberCards, GameObject card)
+    {
+        for(int i = 0; i < numberCards; i++)
+        {
+            for(int j=0; j < players.Length; j++)
+            {
+                if(this.players[j].gameObject.name == "p_1")
+                {
+                    card.GetComponent<CardBehavior>().isPlayer = true;
+                    card.GetComponent<CardBehavior>().isNotPlayer = false;               
+                }
+                else
+                {
+                    card.GetComponent<CardBehavior>().isPlayer = false;
+                    card.GetComponent<CardBehavior>().isNotPlayer = true;
+                    card.GetComponent<CardBehavior>().isDeck = false;
+
+                }
+                Instantiate(card, players[j].transform.position, players[j].transform.rotation);
+                cardsInTable += 1;
+            }
+        }
+    }
+    public void SpawnDeck(int numberCards, GameObject card)
+    {
+        for (int i = 0; i < numberCards - cardsInTable; i++)
+        {
+                if (this.deck.gameObject.name == "deck")
+                {
+                    card.GetComponent<CardBehavior>().isDeck = true;
+                    card.GetComponent<CardBehavior>().isNotPlayer = false;
+                }
+                GameObject obj = Instantiate(card, deck.transform.position, deck.transform.rotation);
+                obj.name = "Card_" + i;
+                cardInDeck.Add(obj);
+        }
+        if(gameBegin)
+        PutTheStartingCard();
+    }
+    public void PutTheStartingCard()
+    {
+        firstInTable = GameObject.Find("Card_0");
+        firstInTable.transform.position = firstInTablePosition.transform.position;
+        firstInTable.GetComponent<CardBehavior>().isDeck = false;
+        firstInTable.GetComponent<CardBehavior>().isTable = true;
+        cardInDeck.Remove(firstInTable);
+        turnTheCard = true;
+        cardsInTable += 1;
+        gameBegin = false;
+    }
+    public void PutInTable()//Apesar do nome, essa função controla as mensagens de passagem de turnos.
+    {
+        switch(idTurn)
+        {
+            case 1:
+                myTurn.SetTrigger("Turn");
+                audioClip = Resources.Load<AudioClip>("turnPass");
+                audioSource.clip = audioClip;
+                audioSource.Play();
+                break;
+            case 2:
+                oponentTurn.SetTrigger("Turn");
+                audioClip = Resources.Load<AudioClip>("turnPass");
+                audioSource.clip = audioClip;
+                audioSource.Play();
+                break;
+        }
+    }
+
+    public void TryCards() //Realiza diversos testes de lógica na mesa de jogo
+    {
+        if (this.state == Turn.PLAYER_2)
+        {
+            Debug.Log("Entra");
+            foreach(GameObject card in oponentHand)
+            {
+                if (this.cardInTable.GetComponent<CardBehavior>().valorCarta == card.GetComponent<CardBehavior>().valorCarta
+                    || this.cardInTable.GetComponent<CardBehavior>().corCarta == card.GetComponent<CardBehavior>().corCarta
+                    || this.cardInTable.GetComponent<CardBehavior>().isSpecial && this.cardInTable.GetComponent<CardBehavior>().corCarta == CardBehavior.CardColor.UNCOLOR
+                    || card.GetComponent<CardBehavior>().isSpecial && card.GetComponent<CardBehavior>().corCarta == CardBehavior.CardColor.UNCOLOR)
+                {
+                    Debug.Log("Valor:" +  card.GetComponent<CardBehavior>().valorCarta + "Cor:" + card.GetComponent<CardBehavior>().corCarta);
+                    this.state = Turn.PLAYER_1;
+                    card.GetComponent<CardBehavior>().canInteract = true;
+                    card.transform.position = cardInTable.transform.position;
+                    oponentHand.Remove(card);
+                    hasPlayed = true;
+                    Debug.Log("Eu tenho");
+                    break;
+                }            
+            }
+            if (!hasPlayed)
+            {
+                GameObject cardBuy = cardInDeck[cardInDeck.Count - 1];
+                cardInDeck.Remove(cardBuy);
+                cardBuy.transform.position = players[1].transform.position;
+                oponentHand.Add(cardBuy);
+                cardBuy.GetComponent<CardBehavior>().isPlayer = false;
+                cardBuy.GetComponent<CardBehavior>().isNotPlayer = true;
+                cardBuy.GetComponent<CardBehavior>().isDeck = false;
+                if (this.cardInTable.GetComponent<CardBehavior>().valorCarta == cardBuy.GetComponent<CardBehavior>().valorCarta
+                   || this.cardInTable.GetComponent<CardBehavior>().corCarta == cardBuy.GetComponent<CardBehavior>().corCarta
+                   || this.cardInTable.GetComponent<CardBehavior>().isSpecial && this.cardInTable.GetComponent<CardBehavior>().corCarta == CardBehavior.CardColor.UNCOLOR
+                   || cardBuy.GetComponent<CardBehavior>().isSpecial && cardBuy.GetComponent<CardBehavior>().corCarta == CardBehavior.CardColor.UNCOLOR)
+                {
+                    Debug.Log("Valor:" + cardBuy.GetComponent<CardBehavior>().valorCarta + "Cor:" + cardBuy.GetComponent<CardBehavior>().corCarta);
+                    this.state = Turn.PLAYER_1;
+                    cardBuy.GetComponent<CardBehavior>().canInteract = true;
+                    cardBuy.transform.position = cardInTable.transform.position;
+                    cardBuy.transform.rotation = cardInTable.transform.rotation;
+                    oponentHand.Remove(cardBuy);
+                    Debug.Log("Eu comprei e tenho");
+                }
+                else
+                {
+                    Debug.Log("Não tenho");
+                    opponentTurn = false;
+                    this.state = Turn.PLAYER_1;
+                }    
+            }
+            Debug.Log("Finalizei minha jogada");
+            hasPlayed = false;
+            opponentTurn = false;
+            this.state = Turn.PLAYER_1;
+            idTurn = 1;
+            PutInTable();
+        }
+    }
+
+    public IEnumerator StateMachine()
+    {
+        Debug.Log("Entrou no State");
+        opponentTurn = true;
+        yield return new WaitForSeconds(3);
+        this.state = Turn.PLAYER_2;
+        Debug.Log("Saiu do State");
+    }
+
+    public IEnumerator BackToMenu()
+    {
+        Debug.Log("Acabou");
+        yield return new WaitForSeconds(5);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+    }
+}
